96b03b82ea7c49b4f28e03603573d00e4fa03436
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-17 18:57:14 +0530

    setting up

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,10 +1,16 @@
 
 import { BrowserRouter  as Router ,Routes,Route } from "react-router-dom"
+import PublicLayout from "./layouts/PublicLayout"
+import Home from "./routes/Home"
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route></Route>
+        <Route element={<PublicLayout/>}>
+        <Route index element={<Home/>}>
+        </Route>
+        
+        </Route>
       </Routes>
     </Router>
   )
--- project/src/layouts/PublicLayout.tsx
+++ project/src/layouts/PublicLayout.tsx
@@ -1,8 +1,13 @@
-
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const PublicLayout = () => {
   return (
-    <div>
+    <div className='w-full'>
+      <Header/>
+      <Outlet/>
+      <Footer/>
       
     </div>
   )
