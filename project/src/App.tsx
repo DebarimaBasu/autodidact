c8a7d9f45af8ef71a6d30917bcb4aacb008b2d35
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-18 14:33:05 +0530

    authentication

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter  as Router ,Routes,Route } from "react-router-dom"
 import PublicLayout from "./layouts/PublicLayout"
 import Home from "./routes/Home"
 import AuthLayout from "./layouts/AuthLayout"
-import SignIn from "./routes/SignIn"
-import SignUp from "./routes/SignUp"
+import SignInPage from "./routes/SignInPage"
+import SignUpPage from "./routes/SignUpPage"
 const App = () => {
   return (
     <Router>
@@ -16,8 +16,8 @@ const App = () => {
 
          {/*public routes */}
          <Route element={<AuthLayout/>}>
-        <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignInPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
         </Route>
 
         {/* protected layout*/ }
--- project/src/layouts/AuthLayout.tsx
+++ project/src/layouts/AuthLayout.tsx
@@ -3,7 +3,11 @@ import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
-    <div className="w-screen h-screen overflow-hidden flex items-center justify-center">
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center background-color: bg-gray-900">
+      <img
+      src="/assets/img/bg.png"
+      className="absolute w-full h-full object-cover opacity-20"
+      alt="picture" />
         <Outlet/>
       
     </div>
