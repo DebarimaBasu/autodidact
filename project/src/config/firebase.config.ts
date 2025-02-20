d18d8051a70db6254ef88da30986e4cc5efdbe1b
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-02-20 18:00:52 +0530

    firebase configuration

--- project/src/config/firebase.config.ts
+++ project/src/config/firebase.config.ts
@@ -1,17 +1,24 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
+
+
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAMtPPTWvZrX3YXAuigYV4Qn4iGEQLcLbk",
+  authDomain: "autodidact-1df11.firebaseapp.com",
+  projectId: "autodidact-1df11",
+  storageBucket: "autodidact-1df11.firebasestorage.app",
+  messagingSenderId: "581543974180",
+  appId: "1:581543974180:web:44617c97faf9abe79c4fb1",
+  measurementId: "G-W8KBJ4EJB3"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+
+
 
 export { db };
